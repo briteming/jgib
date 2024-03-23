6de2353133a5d3165a2bb2dd4724970a4b587be4
Jim Huang <jim.huang112@gmail.com>
2024-03-23 14:16:26 +0800

    :package: chore: add drop console when build

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;
